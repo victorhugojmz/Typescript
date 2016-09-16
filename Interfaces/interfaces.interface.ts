ba25c0c031f5c6290ca0f51d2e0034f97d6bb651
Victor Hugo Barrios Jiménez <vhbj149601@gmail.com>
2016-09-16 18:29:55 -0500

    Add files via upload

---
+++ Interfaces/interfaces.interface.ts
@@ -0,0 +1,15 @@
+export interface Post 
+{
+    postId: number;
+    titulo: string;
+    body:  string;
+    user:  string;
+    userId?: number;
+}
+export interface Movements 
+{
+    x: number;
+    y: number;
+    z: number;
+ }
+
